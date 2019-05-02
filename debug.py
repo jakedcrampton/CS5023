db3a939d1e9988d04ae3a2b97e94e7745960fb5b
Jake Crampton <jake.crampton@ossm.edu>
2019-05-02 01:32:24 -0500

    fixed debug

--- debug.py
+++ debug.py
@@ -4,11 +4,12 @@ import time
 
 if __name__ == '__main__':
 	while(True):
-		print("searching")
 		if(os.stat("reqs.txt").st_size != 0):
 			file=open("reqs.txt")
 			lines=file.readlines();
-			os.system(lines[0])
+			print(lines[0])
 			os.system("rm reqs.txt; touch reqs.txt")
 			file.close()
+		else:
+			print("-----")
 		time.sleep(3)
