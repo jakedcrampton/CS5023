7abcb41ad33e4a6e350ea2af455be3687d177dc9
Jake Crampton <jake.crampton@ossm.edu>
2019-05-01 22:26:38 -0500

    small edits

--- giveTour.py
+++ giveTour.py
@@ -24,7 +24,7 @@ import os
 # Helper Methods  #
 #############
 def getPosition():
-	reversedLines=reversed(open("filename").readlines())
+	reversedLines=reversed(open("position.txt").readlines())
 	print(reversedLines[3][27-31])
 def dist(x1,y1,x2,y2):
 	return math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
